78dfa7e75b7f1f10209176585e8c676322f95a2f
Emre DEMİRÖZ <emredemiroz173@gmail.com>
2025-08-24 16:04:57 +0300

    mesaj = input("Bir mesaj girin: ") print("Merhaba " + mesaj)

---
+++ degiskenler.py
@@ -0,0 +1,37 @@
+# Sayısal veri türleri
+tam_sayi = 10  # Integer (int)
+ondalik_sayi = 3.14  # Float
+karma_sayi = 2 + 3j  # Complex
+
+# Metin veri türü
+metin = "Merhaba, Python!"  # String (str)
+
+# Boolean veri türü
+dogru_mu = True  # Boolean (bool)
+
+# Liste veri türü
+liste = [1, 2, 3, "Python", 4.5]  # List
+
+# Demet veri türü
+demet = (10, 20, 30, "Merhaba")  # Tuple
+
+# Sözlük veri türü
+sozluk = {"ad": "Emre", "yas": 25}  # Dictionary (dict)
+
+# Küme veri türü
+kume = {1, 2, 3, 4, 5}  # Set
+
+# NoneType veri türü
+bos = None  # NoneType
+
+# Değişkenleri kullanma
+print(f"Tamsayı: {tam_sayi}")
+print(f"Ondalık sayı: {ondalik_sayi}")
+print(f"Karmaşık sayı: {karma_sayi}")
+print(f"Metin: {metin}")
+print(f"Boolean: {dogru_mu}")
+print(f"Liste: {liste}")
+print(f"Demet: {demet}")
+print(f"Sözlük: {sozluk}")
+print(f"Küme: {kume}")
+print(f"NoneType: {bos}") 
